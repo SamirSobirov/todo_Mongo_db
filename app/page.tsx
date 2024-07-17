be1fa408e5d512384ff474d82318c9e517995f04
Sobirov S <sobiroff23@gmail.com>
2024-07-18 02:03:54 +0500

    fixed

--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,6 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <button
       onClick={postData}>
-        onc
         create</button>
     </main>
   )
