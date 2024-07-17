ffbda2e28eaff6d5a43810e26cef6d15ae10127f
Sobirov S <sobiroff23@gmail.com>
2024-07-17 15:00:32 +0500

    fixed bags

---
+++ app/api/users/route.ts
@@ -0,0 +1,9 @@
+import dbConnect from "../../../lib/dbConnect";
+import { NextRequest, NextResponse } from "next/server";
+
+export default async function handlers(req: NextRequest, res: NextResponse) {
+    
+    req.method
+
+
+}
